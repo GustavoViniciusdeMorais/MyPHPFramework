dbe7294b50daaa3f7d1aaedb2cce4aba65a45a5c
gustavoviniciusdemorais <gustavocop14dos@gmail.com>
2022-12-03 12:28:43 -0300

    route system working

--- app.php
+++ app.php
@@ -2,4 +2,17 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$router = new Gustavovinicius\Mkfig\Routes\Router;
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+// print_r(json_encode([$pathInfo, $requestMethod]));
+// exit;
+
+$router = new Gustavovinicius\Mkfig\Routes\Router($pathInfo, $requestMethod);
+
+$router->get('/test/{id}', function ($params) {
+    return ' asdfasdfasd ddd ' . $params[1];
+});
+
+$result = $router->run();
+var_dump($result['callback']($result['params']));
--- src/Routes/Router.php
+++ src/Routes/Router.php
@@ -5,10 +5,14 @@ namespace Gustavovinicius\Mkfig\Routes;
 class Router
 {
     private $collection;
+    private $path;
+    private $method;
 
-    public function __construct()
+    public function __construct(string $path, string $method)
     {
         $this->collection = new RouterCollection();
+        $this->path = $path;
+        $this->method = $method;
     }
 
     public function get($path, $callback)
@@ -24,5 +28,45 @@ class Router
     public function request($method, $path, $callback)
     {
         $this->collection->add($method, $path, $callback);
+    }
+
+    public function run()
+    {
+        $data = $this->collection->filter($this->method);
+
+        foreach ($data as $key => $value) {
+            $result = $this->checkUrl($key, $this->path);
+
+            $callback = $value;
+            if ($result['result']) {
+                break;
+            }
+        }
+
+        if (!$result['result']) {
+            $callback = null;
+        }
+
+        return [
+            'params' => $result['params'],
+            'callback' => $callback
+        ];
+    }
+
+    public function checkUrl(string $needle, $subject)
+    {
+        preg_match_all('/\{([^\}]*)\}/', $needle, $variables);
+
+        $regex = str_replace('/', '\/', $needle);
+
+        foreach ($variables[1] as $key => $variable) {
+            $as = explode(':', $variable);
+            $replacement = $as[1] ?? '([a-zA-Z0-9\-\_\s]+)';
+            $regex = str_replace($variables[$key], $replacement, $regex);
+        }
+        $regex = preg_replace('/{([a-zA-Z]+)}/', '([a-zA-Z0-9+])', $regex);
+        $result = preg_match('/^' . $regex . '$/', $subject, $params);
+
+        return compact('result', 'params');
     }
 }
